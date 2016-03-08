95aad8df763bcf39ebe5659b6f200d57cddc9fdc
Joshua Peeling <joshisapeeling@yahoo.com>
2016-03-08 12:13:49 -0500

    Initial Commit

---
+++ gitTestAppTests/gitTestAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  gitTestAppTests.swift
+//  gitTestAppTests
+//
+//  Created by Joshua Peeling on 3/8/16.
+//  Copyright © 2016 Joshua Peeling. All rights reserved.
+//
+
+import XCTest
+@testable import gitTestApp
+
+class gitTestAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
